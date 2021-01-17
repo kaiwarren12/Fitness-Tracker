1578beedda72880a360b5b16ad33813b7153d617
Kai Warren <kaiwarren12@yahoo.com>
2021-01-16 20:43:23 -0800

    added server.js

--- Develop/public/exampleModel.js
+++ Develop/public/exampleModel.js
@@ -4,24 +4,30 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     Name: {
         type: String,
+        required: true,
     },
 
     type: {
         type: String,
+        required: true,
     },
 
     weight: {
         type: Number,
+        required: true,
     },
 
     sets: {
         type: Number,
+        required: true,
     },
     reps: {
         type: Number,
+        required: true,
     },
     duration: {
         type: Number,
+        required: true,
     },
 });
 
